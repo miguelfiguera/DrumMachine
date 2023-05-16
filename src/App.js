5c0839a882991622f591bea165aa345d93d99f6d
Miguel Figuera <miguelqui725@gmail.com>
2023-05-16 19:41:29 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import DrumMachine from './DrumMachine';
 function App() {
   return (
-    <div className="App">
+    <div className="App container-fluid" style={{background:' linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)'}}>
       <DrumMachine />
     </div>
   );
--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -7,39 +7,57 @@ export default function DrumMachine() {
   function instancesOfSound(element) {
     const audio = document.getElementById(element.letter);
     setInstrumentPlaying(`${element.sound}`);
-    audio.currentTime=0
-    audio.play()
+    audio.currentTime = 0;
+    audio.play();
   }
 
-  useEffect(()=>{
-    document.addEventListener('keydown',(event)=>{
-      const toUpper=event.key.toUpperCase()
-      const filtered=objects.filter(element=>element.letter===toUpper)
-      if(filtered.length!==0)instancesOfSound(filtered[0])
-    })
-  },[])
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      const toUpper = event.key.toUpperCase();
+      const filtered = objects.filter((element) => element.letter === toUpper);
+      if (filtered.length !== 0) instancesOfSound(filtered[0]);
+    });
+  }, []);
 
   return (
-    <div id="drum-machine" className=" container border border-secondary border-end rounded-3 position-absolute top-50 start-50 translate-middle" style={{maxWidth:'45rem'}}>
-     <div className="row align-items-start">
-      <div id="display" className="col  text-center " style={{maxWidth:'15rem'}}> <strong> <p className="text-center fs-4 border border-secondary bg-dark text-light" style={{translate:'0 300%'}}>{instrumentPlaying}</p></strong></div>
-      <div className="col border border-start d-flex p-2 flex-wrap justify-content-center">
-      {objects.map((e) => {
-        return (
-          <div key={e.keyCode}
-          style={{minWidth:'5rem'}}
-            onClick={() => {
-              instancesOfSound(e);
-            }}
-            className="drum-pad btn btn-dark mx-4 my-4"
-            id={e.sound}
-          >
-            {e.letter}
-            <audio className="clip" src={e.audio} id={e.letter}></audio>
-          </div>
-        );
-      })}
-      </div>
+    <div
+      id="drum-machine"
+      className=" container border border-secondary border-end rounded-3 position-absolute top-50 start-50 translate-middle"
+      style={{ maxWidth: "45rem",background:'linear-gradient(225deg, rgba(84,141,232,1) 36%, rgba(209,6,244,1) 71%)' }}
+    >
+      <div className="row align-items-start" >
+        <div
+          id="display"
+          className="col  text-center "
+          style={{ maxWidth: "15rem" }}
+        >
+         { instrumentPlaying===''? null:<strong>
+            <p
+              className="text-center fs-4 border border-secondary bg-dark text-light"
+              style={{ translate: "0 300%" }}
+            >
+              {instrumentPlaying}
+            </p>
+          </strong>}
+        </div>
+        <div className="col border border-start d-flex p-2 flex-wrap justify-content-center">
+          {objects.map((e) => {
+            return (
+              <div
+                key={e.keyCode}
+                style={{ minWidth: "5rem" }}
+                onClick={() => {
+                  instancesOfSound(e);
+                }}
+                className="drum-pad btn btn-dark mx-4 my-4"
+                id={e.sound}
+              >
+                {e.letter}
+                <audio className="clip" src={e.audio} id={e.letter}></audio>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
